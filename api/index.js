964d3b1abaa73dbf06b2330ed4923ddab4ceaef4
Revilum <revilum.hi@gmail.com>
2021-02-09 12:54:45 +0100

    refinded ranking with date and did some more work on the scoreboard

--- api/index.js
+++ api/index.js
@@ -2,29 +2,37 @@ const HLTV = require('hltv-api').default
 const express = require('express')
 const { HLTV: HLTV2 } = require("hltv")
 const fs = require('fs')
+const https = require('https');
+var proxy = require('express-http-proxy')
 
 const app = express()
 
-app.get('/api/ranking', async (req, res) => {
-  const ranking= await HLTV2.getTeamRanking()
+app.get('/ranking', async (req, res) => {
+//  const ranking= await HLTV2.getTeamRanking()
+	const ranking = await HLTV2.getOngoingEvents()
   res.json(ranking)
 })
 
-app.get('/api/ranking/:country', async (req, res) => {
+app.get('/ranking/:country', async (req, res) => {
   const parameters = new Object()
   parameters.country = req.params.country
   res.json(await HLTV2.getTeamRanking(parameters))
 })
 
-app.get('/api/ranking/:day/:month/:year', async (req, res) => {
-  const parameters = new Object()
-  parameters.year = req.params.year
-  parameters.month = req.params.month
-  parameters.day = req.params.day
+app.get('/ranking/:day/:month/:year', async (req, res) => {
+	const parameters = new Object()
+	var months = ['january','february','march','april','may','june','july','august','september','october','november','december']
+	var date = new Date((new Date(req.params.day + " " + req.params.month + " " + req.params.year)).getTime() + (2 * 60 * 60 * 1000))
+		while(date.getDay() != 1) {
+			date = new Date(date.getTime() - 86400000)
+	}
+	parameters.year = date.getFullYear()
+	parameters.month = months[date.getMonth()]
+	parameters.day = date.getDate()
   res.json(await HLTV2.getTeamRanking(parameters))
 })
 
-app.get('/api/player/:name', async (req, res) => {
+app.get('/player/:name', async (req, res) => {
   const parameters = new Object()
   parameters.name = req.params.name
   const playerInfo = HLTV2.getPlayerByName(parameters).then(res => {
@@ -33,57 +41,57 @@ app.get('/api/player/:name', async (req, res) => {
   res.json(await playerInfo)
 })
 
-app.get('/api/playerstats/:id', async (req, res) => {
+app.get('/playerstats/:id', async (req, res) => {
   const parameters = new Object()
   parameters.id = req.params.id
   const playerStats = await HLTV2.getPlayerStats(parameters)
   res.json(playerStats)
 })
 
-app.get('/api/upcommingevents', async (req, res) => {
+app.get('/upcommingevents', async (req, res) => {
   const upcommingevents = await HLTV2.getEvents()
   res.json(upcommingevents)
 })
 
-app.get('/api/ongoingevents', async (req, res) => {
+app.get('/ongoingevents', async (req, res) => {
   const ongoingevents = await HLTV2.getOngoingEvents()
   res.json(ongoingevents)
 })
 
-app.get('/api/event/:id', async (req, res) => {
+app.get('/event/:id', async (req, res) => {
   const parameters = new Object()
   parameters.id = req.params.id
   const event = await HLTV2.getEvent(parameters)
   res.json(event)
 })
 
-app.get('/api/match/:id', async (req, res) => {
+app.get('/match/:id', async (req, res) => {
   const parameters = new Object()
   parameters.id = req.params.id
   const match = await HLTV2.getMatch(parameters)
   res.json(match)
 })
 
-app.get('/api/matchstats/:id', async (req, res) => {
+app.get('/matchstats/:id', async (req, res) => {
   const parameters = new Object()
   parameters.id = req.params.id
   const matchstats = await HLTV2.getMatchStats(parameters)
   res.json(matchstats)
 })
 
-app.get('/api/matchesstats', async (req, res) => {
+app.get('/matchesstats', async (req, res) => {
   const matchesstats = await HLTV2.getMatchesStats({startDate: '2017-07-10', endDate: '2017-07-18'})
   res.json(matchesstats)
 })
 
-app.get('/api/matchmapstats/:id', async (req, res) => {
+app.get('/matchmapstats/:id', async (req, res) => {
   const parameters = new Object()
   parameters.id = req.params.id
   const matchmapstats = await HLTV2.getMatchStats(parameters)
   res.json(matchmapstats)
 })
 
-app.get('/api/streams/', async (req, res) => {
+app.get('/streams/', async (req, res) => {
   const streams = await HLTV2.getStreams()
   res.json(streams)
 })
@@ -95,7 +103,7 @@ app.get('/team/:id', async (req, res) => {
   res.json(team)
 })
 
-app.get('/api/teamstats/:id/:currentroster', async (req, res) => {
+app.get('/teamstats/:id/:currentroster', async (req, res) => {
   const parameters = new Object()
   parameters.currentRosterOnly = req.params.currentroster
   parameters.id = req.params.id
@@ -103,7 +111,7 @@ app.get('/api/teamstats/:id/:currentroster', async (req, res) => {
   res.json(teamstats)
 })
 
-app.get('/api/teamstats/:id/:currentroster/startdate/:startdate', async (req, res) => {
+app.get('/teamstats/:id/:currentroster/startdate/:startdate', async (req, res) => {
   const parameters = new Object()
   parameters.currentRosterOnly = req.params.currentroster
   parameters.id = req.params.id
@@ -113,7 +121,7 @@ app.get('/api/teamstats/:id/:currentroster/startdate/:startdate', async (req, re
   res.json(teamstats)
 })
 
-app.get('/api/teamstats/:id/:currentroster/enddate/:enddate', async (req, res) => {
+app.get('/teamstats/:id/:currentroster/enddate/:enddate', async (req, res) => {
   const parameters = new Object()
   parameters.currentRosterOnly = req.params.currentroster
   parameters.id = req.params.id
@@ -122,7 +130,7 @@ app.get('/api/teamstats/:id/:currentroster/enddate/:enddate', async (req, res) =
   res.json(teamstats)
 })
 
-app.get('/api/teamstats/:id/:currentroster/timeframe/:startdate/:enddate', async (req, res) => {
+app.get('/teamstats/:id/:currentroster/timeframe/:startdate/:enddate', async (req, res) => {
   const parameters = new Object()
   parameters.currentRosterOnly = req.params.currentroster
   parameters.id = req.params.id
@@ -132,42 +140,48 @@ app.get('/api/teamstats/:id/:currentroster/timeframe/:startdate/:enddate', async
   res.json(teamstats)
 })
 
-app.get('/api/scoreboard/:id', async (req, res) => {
+var onLogUpdatePrevious
+var onLogUpdateCurrent
+var onScoreboardUpdatePrevious
+var onScoreboardUpdateCurrent
+
+app.get('/scoreboard/:id', async (req, res) => {
 		HLTV2.connectToScorebot({
 			id: req.params.id, 
-			onScoreboardUpdate: (data, done) => {
-			console.log("onScoreboardUpdate received")
-			console.dir(data, {depth: null})
-//			fs.writeFile("./scoreboard/" + Date.now() + ".onScoreboardUpdate", JSON.stringify(data, null, 4), (err) => { 
-				if (err) { 
-					console.log(err); 
+			onLogUpdate: (data, done) => {
+			if (onLogUpdatePrevious == undefined) {
+					onLogUpdatePrevious = data
+					fs.writeFile("./scoreboard/" + Date.now() + ".onLogUpdate" + ".json", JSON.stringify(data, null, 4), (err) => {})
 				}
-			}, onLogUpdate: (data, done) => {
-				console.log("onLogUpdate received")
-				console.dir(data, {depth: null})
-//				fs.writeFile("./scoreboard/" + Date.now() + ".onLogUpdate", JSON.stringify(data, null, 4), (err) => { 
-					if (err) { 
-						console.log(err); 
+				onLogUpdateCurrent = data
+				if (onLogUpdatePrevious != onLogUpdateCurrent) {
+					console.log("onLogUpdate received")
+					fs.writeFile("./scoreboard/" + Date.now() + ".onLogUpdate" + ".json", JSON.stringify(data, null, 4), (err) => {})
+					onLogUpdatePrevious = onLogUpdateCurrent
+				}
+			}, onScoreboardUpdate: (data, done) => {
+					if (onScoreboardUpdatePrevious == undefined) {
+					onScoreboardUpdatePrevious = data
+					fs.writeFile("./scoreboard/" + Date.now() + ".onScoreboardUpdate" + ".json", JSON.stringify(data, null, 4), (err) => {})
 					}
+				onScoreboardUpdateCurrent = data
+				if (onScoreboardUpdatePrevious != onScoreboardUpdateCurrent) {
+					console.log("onScoreboardUpdate received")
+					fs.writeFile("./scoreboard/" + Date.now() + ".onScoreboardUpdate" + ".json", JSON.stringify(data, null, 4), (err) => {})
+					onScoreboardUpdatePrevious = onScoreboardUpdateCurrent
+				}
 			}, onFullLogUpdate: (data, done) => {
 				console.log("onFullLogUpdate received")
-				console.dir(data, {depth: null})
-//				fs.writeFile("./scoreboard/" + Date.now() + ".onFullLogUpdate", JSON.stringify(data, null, 4), (err) => { 
+//				console.dir(data, {depth: null})
+/*				fs.writeFile("./scoreboard/" + Date.now() + ".onFullLogUpdate", JSON.stringify(data, null, 4), (err) => { 
 					if (err) { 
-				console.log(err); 
+				console.log(err)*/
 				}
-			}, onConnect: (data, done) => {
-				console.log("onConnect received")
-				console.dir(data, {depth: null})
-			}, onDisconnect: (data) => {
-				console.log("onDisconnect received")
-				console.dir(data, {depth: null})
-			}
 		})
 })
 
 //broken
-app.get('/api/playerranking/', async (req, res) => {
+app.get('/playerranking/', async (req, res) => {
 //  const parameters = new Object()
 //  parameters.startDate = req.params.startdate
 //  parameters.endDate = req.params.enddate
@@ -180,40 +194,30 @@ app.get('/', async (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/api/news', async (req, res) => {
+app.get('/news', async (req, res) => {
   const news = await HLTV.getNews()
   res.json(news)
 })
 
-app.get('/api/results', async (req, res) => {
+app.get('/results', async (req, res) => {
   const results = await HLTV.getResults()
   res.json(results)
 })
 
-app.get('/api/matches', async (req, res) => {
+app.get('/matches', async (req, res) => {
   const matches = await HLTV.getMatches()
   res.json(matches)
 })
 
-app.get('/api/:matchId(*)', async (req, res) => {
+app.get('/:matchId(*)', async (req, res) => {
 	const { matchId } = req.params
 	const stats = await HLTV.getStatsByMatchId(matchId)
 	res.json(stats)
 })
 
-//const privateKey = fs.readFileSync(`/etc/letsencrypt/live/revilum.com/privkey.pem`, 'utf8', () => {})
-//const certificate = fs.readFileSync(`/etc/letsencrypt/live/revilum.com/fullchain.pem`, 'utf8', () => {})
-//const ca = fs.readFileSync(`/etc/letsencrypt/live/revilum.com/chain.pem`, 'utf8', () => {})
-//const credentials = {
-//  key: privateKey,
-//  cert: certificate,
-//  ca: ca
-//}
-//const httpsServer = https.createServer(credentials, app);
-
-app.use(function(req, res, next){
+app.use (function(req, res, next){
     res.setTimeout(500000, function(){
-        // call back function is called when request timed out.
+        res.send("Timeout")
     });
     next();
 });
@@ -221,7 +225,3 @@ app.use(function(req, res, next){
 app.listen(3000, () => {
   console.log('Listening on port 3000...')
 })
-//httpsServer.listen(3001, () => {
-//  console.log('HTTPS Server running on port 3001');
-//})
-module.exports = app
