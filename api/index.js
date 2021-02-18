db92b84dcaa472a5144d649c2e91e9e52399881d
Nils Jacobs <revilum.hi@gmail.com>
2021-02-18 10:21:30 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { HLTV } = require("hltv")
 const fs = require("fs")
 
+var fd = fs.openSync(filepath, 'w', (err) => {})
 const app = express()
 const enableWs = require('express-ws')(app)
 var matchesFile = fs.readFileSync('matches.json', 'utf8', (err) => {})
