de443ea1eb27ee37129814839d51b21bb8da69ed
Nils Jacobs <niljaco.pi@gmail.com>
2022-01-11 14:10:49 +0100

    updated dependencies

--- api/index.js
+++ api/index.js
@@ -8,11 +8,13 @@ const app = express()
 app.use(bodyParser.json())
 
 async function reportError(err, func, opt) {
-	return await axios.post('https://debug.revilum.com', {
+	var result = await axios.post('https://debug.revilum.com', {
 		"error": err.toString(),
 		"function": func,
 		"options": opt
 	})
+	return result.data
+	
 }
 
 app.post("/api/getMatch", async (req, res) => {
@@ -20,8 +22,8 @@ app.post("/api/getMatch", async (req, res) => {
 		var response = await HLTV.getMatch(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getMatch", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getMatch", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -30,8 +32,8 @@ app.post("/api/getMatches", async (req, res) => {
 		var response = await HLTV.getMatches(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getMatches", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getMatches", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -40,8 +42,8 @@ app.post("/api/getMatchesStats", async (req, res) => {
 		var response = await HLTV.getMatchesStats(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getMatchesStats", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getMatchesStats", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -50,8 +52,8 @@ app.post("/api/getMatchStats", async (req, res) => {
 		var response = await HLTV.getMatchStats(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getMatchStats", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getMatchStats", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -60,8 +62,8 @@ app.post("/api/getMatchMapStats", async (req, res) => {
 		var response = await HLTV.getMatchMapStats(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getMatchMapStats", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getMatchMapStats", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -70,8 +72,8 @@ app.post("/api/getStreams", async (req, res) => {
 		var response = await HLTV.getStreams(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getStreams", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getStreams", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -80,8 +82,8 @@ app.post("/api/getRecentThreads", async (req, res) => {
 		var response = await HLTV.getRecentThreads(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getRecentThreads", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getRecentThreads", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -90,8 +92,8 @@ app.post("/api/getTeamRanking", async (req, res) => {
 		var response = await HLTV.getTeamRanking(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getTeamRanking", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getTeamRanking", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -100,8 +102,8 @@ app.post("/api/getTeam", async (req, res) => {
 		var response = await HLTV.getTeam(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getTeam", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getTeam", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -110,8 +112,8 @@ app.post("/api/getTeamByName", async (req, res) => {
 		var response = await HLTV.getTeamByName(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getTeamByName", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getTeamByName", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -120,8 +122,8 @@ app.post("/api/getTeamStats", async (req, res) => {
 		var response = await HLTV.getTeamStats(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getTeamStats", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getTeamStats", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -130,8 +132,8 @@ app.post("/api/getPlayer", async (req, res) => {
 		var response = await HLTV.getPlayer(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getPlayer", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getPlayer", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -140,8 +142,8 @@ app.post("/api/getPlayerByName", async (req, res) => {
 		var response = await HLTV.getPlayerByName(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getPlayerByName", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getPlayerByName", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -150,8 +152,8 @@ app.post("/api/getPlayerStats", async (req, res) => {
 		var response = await HLTV.getPlayerStats(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getPlayerStats", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getPlayerStats", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -160,8 +162,8 @@ app.post("/api/getPlayerRanking", async (req, res) => {
 		var response = await HLTV.getPlayerRanking(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getPlayerRanking", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getPlayerRanking", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -170,8 +172,8 @@ app.post("/api/getEvents", async (req, res) => {
 		var response = await HLTV.getEvents(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getEvents", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getEvents", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -180,8 +182,8 @@ app.post("/api/getEvent", async (req, res) => {
 		var response = await HLTV.getEvent(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getEvent", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getEvent", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -190,8 +192,8 @@ app.post("/api/getEventByName", async (req, res) => {
 		var response = await HLTV.getEventByName(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getEventByName", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getEventByName", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -200,8 +202,8 @@ app.post("/api/getPastEvents", async (req, res) => {
 		var response = await HLTV.getPastEvents(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getPastEvents", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getPastEvents", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -210,8 +212,8 @@ app.post("/api/getResults", async (req, res) => {
 		var response = await HLTV.getResults(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getResults", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getResults", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
@@ -220,27 +222,18 @@ app.post("/api/getNews", async (req, res) => {
 		var response = await HLTV.getNews(req.body)
 		res.json(response)
 	} catch (err) {
-		reportError(err, "getNews", req.body)
-		res.status(400).send(err.toString())
+		var errorId = await reportError(err, "getNews", req.body)
+		res.status(400).send({error: err.toString(), id: errorId})
 	}
 })
 
-app.post("/api/getNews", async (req, res) => {
-	try {
-		var response = await HLTV.getNews(req.body)
-		res.json(response)
-	} catch (err) {
-		reportError(err, "getNews", req.body)
-		res.status(400).send(err.toString())
-	}
-})
+
 
 app.get("/api/test", async (req, res) => {
-	var ja =await HLTV.getTeamStats({ id: 6137 })
+	var ja = await HLTV.getTeamStats({ id: 6137 })
 	res.json(ja)
 })
 
 var server = app.listen(3000, () => {
 	console.log('Listening on port 3000...')
 })
-
